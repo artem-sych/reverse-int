0771bcfdf72d934d345072bf04ade7f5813bec47
unknown <artem.sych1989@gmail.com>
2022-01-16 12:27:58 +0300

    test task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports =
+    function reverse(n) {
+        let arrNumb = n.toString().split('');
+        if (arrNumb[0] == '-') {
+            let result = +('-' + arrNumb.slice(1).reverse().join(''));
+            return result;
+        } else {
+            return arrNumb.reverse().join('');
+        }
+    };
